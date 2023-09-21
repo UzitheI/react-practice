926de12d1c38619d920f3b7e06b0aca1fcad54ee
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-09-22 04:33:37 +0545

    First prac

---
+++ index.js
@@ -0,0 +1,19 @@
+
+ReactDOM.render((
+<div class="flex justify-between align-center max-width-1400px">
+    <div>
+        <img href="https://pixabay.com/get/g6c899541c78e0e7a615719491044e173b3638e3779c76e2159dce2a42ac5313cdd433d5c6bfd4c2e6f438ac17e44eb0c_1280.png" alt="img"></img>
+
+    </div>
+    <div class="justify-between">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Secularismo</a>
+        <a href="">Pricing</a>
+    </div>
+    <div>
+        <button>Email</button>
+        <button>Events</button>
+    </div>
+</div>
+),document.getElementById("root"))
