98d4adb82d9913ed9394441f59bbf0308e4fc59d
Ujjwal Puri <ujjwalpuri221@gmail.com>
2023-09-22 15:26:20 +0545

    first commit

---
+++ secondprac/index.js
@@ -0,0 +1,17 @@
+const navbar=(
+    <div>
+    <h1>Cookie Monster</h1>
+    <div>
+    <ul className="sinkoman">
+    <li>home</li>
+    <li>home</li>
+    <li>home</li>
+    <li>home</li>
+    </ul>
+    </div>
+    <p>what we do here is go back</p>
+    </div>
+)
+ReactDOM.render(
+    navbar,document.getElementById("root")
+)
